9f88be9dd3cfadc3845a7b4cbef5da26ffdf1f00
mzidanrifqirtkj <mzidanrifqirtkj@gmail.com>
2025-10-05 17:09:46 +0700

    fix:gatsby/meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Muhammad Zidan Rifqi Ramadhan',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Muhammad Zidan Rifqi Ramadhan is a Full-Stack Developer specializing in building scalable web applications with Laravel, React, and modern technologies. Experienced in backend architecture, authentication systems, and database optimization.',
+    siteUrl: 'https://mzidanrifqirtkj.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@mzidanrifqirtkj',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Muhammad Zidan Rifqi Ramadhan',
+        short_name: 'Zidan',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
