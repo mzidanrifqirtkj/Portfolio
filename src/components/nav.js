df279949d8c3b55e3a57dde3b63237d2297ed550
mzidanrifqirtkj <mzidanrifqirtkj@gmail.com>
2026-01-03 13:29:26 +0700

    Update page context in gatsby-node.js, change resume link in nav.js, and add Vercel configuration files

--- src/components/nav.js
+++ src/components/nav.js
@@ -208,7 +208,11 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a
+      className="resume-button"
+      href="https://drive.google.com/file/d/1nQ_jicyY7Xy5F9cL1-zKlhwIvjqg15t-/view?usp=sharing"
+      target="_blank"
+      rel="noopener noreferrer">
       Resume
     </a>
   );
